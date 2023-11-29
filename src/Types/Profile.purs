da5dce8238387a50173af3a326e3bf461c7531e5
ichi-h <85932406+ichi-h@users.noreply.github.com>
2023-11-30 01:06:31 +0900

    Update profile.

--- src/Types/Profile.purs
+++ src/Types/Profile.purs
@@ -1,29 +1,21 @@
 module Types.Profile where
 
 import Prelude
-
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
 import Types.Language (Language)
 
-newtype Profile = Profile
-  {
-    "1_name" :: String,
-    "2_location" :: String,
-    "3_favorites" ::
-      {
-        "3.1_languages" :: Array Language,
-        "3.2_writers" :: Array String,
-        "3.3_philosophers" :: Array String,
-        "3.4_books" :: Array String,
-        "3.5_musics" :: Array String,
-        "3.6_subcultures" :: Array String
-      },
-    "4_links" :: Array
-      {
-        site :: String,
-        url :: String
-      }
+newtype Profile
+  = Profile
+  { "1_name" :: String
+  , "2_location" :: String
+  , "3_languages" :: Array Language
+  , "4_favorites" :: Array String
+  , "5_links" ::
+      Array
+        { site :: String
+        , url :: String
+        }
   }
 
 derive instance genericProfile :: Generic Profile _
