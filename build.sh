303f13811f2292931c166398fcc883a8b0fb5dfa
ichi-h <85932406+ichi-h@users.noreply.github.com>
2026-01-13 11:48:54 +0900

    feat: update build script to use sed

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-template=$(cat ./README.template.md)
-mainPurs=$(cat ./src/Main.purs)
-
-template=${template//"{{ PROFILE_CODE }}"/$mainPurs}
-
-echo "$template" > ./out/README.md
+cat ./README.template.md \
+  | sed "/{{ PROFILE_CODE }}/r ./purs/src/Main.purs" \
+  | sed "/{{ PROFILE_CODE }}/d" \
+  > ./out/README.md
